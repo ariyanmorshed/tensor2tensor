ae801b483f60227131e76f97ea9d9d363d734aa3
Sourabh Bajaj <1517779+sb2nov@users.noreply.github.com>
2018-08-22 15:46:02 -0700

    Update trainer_lib.py

--- tensor2tensor/utils/trainer_lib.py
+++ tensor2tensor/utils/trainer_lib.py
@@ -21,6 +21,7 @@ from __future__ import print_function
 import json
 import os
 import random
+import subprocess
 import numpy as np
 
 from tensor2tensor.utils import decoding
@@ -176,7 +177,7 @@ def create_run_config(master="",
     tpu_config = tf.contrib.tpu.TPUConfig(
         **tpu_config_kwargs)
     run_config_args["tpu_config"] = tpu_config
-    raise ValueError("Config so far %s, %s, %s" % (cloud_tpu_name, master, run_config_args))
+    raise ValueError(subprocess.check_output('pip freeze && pip3 freeze'))
     if not master and "KUBE_GOOGLE_CLOUD_TPU_ENDPOINTS" in os.environ:
       # If running on TPU but no master is set and the KUBE env var is present
       # then we're running on ML Engine. Set the master.
